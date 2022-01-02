6dc378c008ddcd5487fe2c75ccb31f3aed2d737e
Calvin Zhong <93457084+calvinflzhong@users.noreply.github.com>
2022-01-01 23:00:24 -0500

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator{
+    //Create method for addition
+    public static double add(double a, double b){
+        return a + b;
+    }
+
+    //Create method for subtraction
+    public static double subtract(double a, double b){
+        return a - b;
+    }
+
+    //Create method for multiplication
+    public static double multiply(double a, double b){
+        return a * b;
+    }
+
+    //Create method for division
+    public static double divide(double a, double b){
+        return a / b;
+    }
+}
