a0259451ae65d3035cbfebbb8814027e7f7c4d09
Calvin Zhong <93457084+calvinflzhong@users.noreply.github.com>
2022-02-13 00:55:14 -0500

    Add files via upload

--- TestCalculator.java
+++ TestCalculator.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class TestCalculator{
     public static void main (String[] args){
         //Declare variable for operator
-        int operator = 1;
+        int operator = -1;
         //Declare variable for output
         double result;
         
@@ -16,8 +16,14 @@ public class TestCalculator{
                     System.out.println("Please choose from the following operations by inputting the number beside it: " +
                             "\nAddition: 1\nSubtraction: 2\nMultiplication: 3\nDivision: 4\nExit: 0\nChoose your operation: ");
                     operator = input.nextInt();
-                    if (operator == 0) 
+                    if (operator == 0) {
+                        System.out.println("Exiting...");
                         System.exit(0);
+                    }    
+                    if(operator<0 || operator>4) {
+                        System.out.println("Please enter numbers 0, 1, 2, 3, or 4");
+                        continue;
+                    }
                     
                     System.out.println("Please enter the two numbers to be calculated using the above operation separated by a space: ");
                     double number1 = input.nextDouble();
